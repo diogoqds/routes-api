9203a74310edf98214640091af2be62a474f19cc
Diogo Queiroz <diogo.qds97@gmail.com>
2021-01-16 15:02:47 -0300

    fix: return the error in the Encode method

--- infra/authentication/jwt.go
+++ infra/authentication/jwt.go
@@ -1,7 +1,6 @@
 package authentication
 
 import (
-	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"os"
 )
@@ -15,9 +14,9 @@ type jsonWebToken struct{}
 func (j jsonWebToken) Encode(body map[string]interface{}) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(body))
 	signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
-	fmt.Println("the secret key is", os.Getenv("JWT_SECRET_KEY"))
+
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 
 	return signedToken, err
