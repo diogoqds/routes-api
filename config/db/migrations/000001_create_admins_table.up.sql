f9d72c29c6e4a34a2c51cf617ec24e41c6649cf8
Diogo Queiroz <diogo.qds97@gmail.com>
2021-01-18 20:41:22 -0300

    refactor: remove unnecessary columns (name, delete_at) from admin table

--- config/db/migrations/000001_create_admins_table.up.sql
+++ config/db/migrations/000001_create_admins_table.up.sql
@@ -1,8 +1,6 @@
 CREATE TABLE admins (
     id              SERIAL PRIMARY KEY,
-    name            VARCHAR(100) NOT NULL,
     email           VARCHAR(100) NOT NULL UNIQUE,
     created_at      TIMESTAMP DEFAULT NOW(),
-    updated_at      TIMESTAMP DEFAULT NOW(),
-    deleted_at      TIMESTAMP
+    updated_at      TIMESTAMP DEFAULT NOW()
 );
