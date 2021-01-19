c0acdc1abdac87d2b67fdef912bf893b6dcd45ae
Diogo Queiroz <diogo.qds97@gmail.com>
2021-01-19 19:27:10 -0300

    feat: implement the query to find an admin by email

--- repositories/admin_repository.go
+++ repositories/admin_repository.go
@@ -1,16 +1,35 @@
 package repositories
 
-import "github.com/diogoqds/routes-challenge-api/entities"
+import (
+	"context"
+	"database/sql"
+	"github.com/diogoqds/routes-challenge-api/entities"
+	"github.com/diogoqds/routes-challenge-api/infra"
+	"log"
+)
 
 type FindAdmin interface {
 	FindByEmail(email string) (*entities.Admin, error)
 }
 
-type AdminRepository struct {}
+type AdminRepository struct{}
 
 func (a AdminRepository) FindByEmail(email string) (*entities.Admin, error) {
-	return nil, nil
+	var admin entities.Admin
+	var ctx context.Context
 
+	err := infra.DB.QueryRowContext(ctx, "SELECT id, email, created_at, updated_at FROM admins WHERE email=?", email).Scan(&admin)
+	switch {
+	case err == sql.ErrNoRows:
+		log.Printf("no admin with email %d\n", email)
+		return nil, err
+	case err != nil:
+		log.Fatalf("query error: %v\n", err)
+		return nil, err
+	default:
+		log.Printf("admin found it")
+		return &admin, nil
+	}
 }
 
 var (
