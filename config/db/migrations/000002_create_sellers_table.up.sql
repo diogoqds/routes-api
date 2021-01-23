e1892733eb84f11ee6abc5a073636d73b910f1cd
Diogo Queiroz <diogo.qds97@gmail.com>
2021-01-23 14:11:24 -0300

    refactor: change the default value of deleted_at column

--- config/db/migrations/000002_create_sellers_table.up.sql
+++ config/db/migrations/000002_create_sellers_table.up.sql
@@ -4,5 +4,5 @@ CREATE TABLE sellers (
     email           VARCHAR(100) NOT NULL UNIQUE,
     created_at      TIMESTAMP DEFAULT NOW(),
     updated_at      TIMESTAMP DEFAULT NOW(),
-    deleted_at      TIMESTAMP DEFAULT NOW()
+    deleted_at      TIMESTAMP DEFAULT NULL
 );
