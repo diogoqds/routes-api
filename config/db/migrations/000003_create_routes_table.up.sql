3ed959d7710d2e5a31b2d148026d824f0fad51bc
Diogo Queiroz <diogo.qds97@gmail.com>
2021-01-27 13:05:06 -0300

    feat: add migration to create the routes table

---
+++ config/db/migrations/000003_create_routes_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE routes (
+    id              SERIAL PRIMARY KEY,
+    name            VARCHAR(100) NOT NULL,
+    bounds          GEOMETRY(Polygon, 28992),
+    seller_id       INTEGER REFERENCES sellers (id),
+    created_at      TIMESTAMP DEFAULT NOW(),
+    updated_at      TIMESTAMP DEFAULT NOW(),
+    deleted_at      TIMESTAMP DEFAULT NULL
+);
